97c2cfb7bb501db2dfaba79246a3610c736c0b07
carbon1423 <bocke034@umn.edu>
2025-05-05 11:50:39 -0500

    added gravity and player movement

--- client/client.c
+++ client/client.c
@@ -11,6 +11,14 @@
 int main() {
     const int FPS = 60;
     const Uint32 frameDelay = 1000 / FPS;  // ~16 ms
+    float vy = 0;
+    float vx = 0;
+    float ax = 0;
+    const float accel = 1.0f;
+    const float max_speed = 10.0f;
+    const float friction = 0.95f;
+    int on_ground = 0;
+    float gravity = 0.5;
 
     SDL_Init(SDL_INIT_VIDEO);
 
@@ -56,35 +64,76 @@ int main() {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) running = 0;
             if (event.type == SDL_KEYDOWN) {
-                switch (event.key.keysym.sym) {
-                    case SDLK_w: player.y -= 10; break;
-                    case SDLK_s: player.y += 10; break;
-                    case SDLK_a: player.x -= 10; break;
-                    case SDLK_d: player.x += 10; break;
-                }
-                struct MovementPacket packet;
-                packet.type = 1;
-                packet.x = player.x;
-                packet.y = player.y;
-                ssize_t sent = send(sock_fd, &packet, sizeof(packet), 0);
-                if (sent < 0) {
-                    perror("send");
-                }
+                if (event.key.keysym.sym == SDLK_w && on_ground) {
+                    vy -= 10;
+                    on_ground = 0;
+                } else if (event.key.keysym.sym == SDLK_s) {
+                    vy += 10;
+                } 
+
+                
             }
+        }
+        const Uint8 *keys = SDL_GetKeyboardState(NULL);
+        ax = 0;
+
+        if (keys[SDL_SCANCODE_A]) {
+            ax = -accel;
+        } else if (keys[SDL_SCANCODE_D]) {
+            ax = accel;
         }
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // black background
         SDL_RenderClear(renderer);
-        struct BroadcastPacket packet;
-        int bytes_recv = recv(sock_fd, &packet, sizeof(packet), 0);
+        struct BroadcastPacket broad_pack;
+        int bytes_recv = recv(sock_fd, &broad_pack, sizeof(broad_pack), 0);
         SDL_Rect other;
         other.w = 50;
         other.h = 50;
 
+        if (!on_ground) {
+            vy += gravity;     // gravity pulls down
+            player.y += vy;    // update position
+        }
+        if (player.y >= 600-player.h) {
+            player.y = 600-player.h;
+            vy = 0;
+            on_ground = 1;
+        }
+        vx += ax;
+
+        // Apply friction when no horizontal input
+        if (ax == 0) {
+            vx *= friction;
+            if (fabs(vx) < 0.1f) vx = 0;
+        }
+
+        // Clamp horizontal speed
+        if (vx > max_speed) {
+            vx = max_speed;
+        }
+        if (vx < -max_speed) {
+            vx = -max_speed;
+        }
+
+        // Move player
+        player.x += vx;
+
+        struct MovementPacket packet;
+        packet.type = 1;
+        packet.x = player.x;
+        packet.y = player.y;
+        ssize_t sent = send(sock_fd, &packet, sizeof(packet), 0);
+        if (sent < 0) {
+            perror("send");
+        }
+
+
+
         if(bytes_recv > 0){
-            for(int i = 0; i < packet.count; i++){
+            for(int i = 0; i < broad_pack.count; i++){
                 if(player_id != i){
-                    other.x = packet.players[i].x;
-                    other.y = packet.players[i].y;
+                    other.x = broad_pack.players[i].x;
+                    other.y = broad_pack.players[i].y;
                     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // blue player
                     SDL_RenderFillRect(renderer, &other);
                 }
